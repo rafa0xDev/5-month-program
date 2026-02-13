1d07b544ac5452f5797dd18daab05c9c1c1ce91d
RohanAlief <rafarafa92653@gmail.com>
2026-02-13 23:12:45 +0700

    feat: generate all possible PIN variations

--- day1/day1.js
+++ day1/day1.js
@@ -2904,3 +2904,35 @@ function greet (name, owner) {
 }
 
 console.log(greet('Daniel', 'Daniel')) // 'Hello boss'
+
+function getPINs(observed) {
+  // TODO: This is your job, detective!
+  const keyPad = {
+    "1" : ["1", "2", "4"],
+    "2" : ["2", "1", "3", "5"],
+    "3" : ["3", "2", "6"],
+    
+    "4": ["4", "1", "5", "7"],
+    "5": ["5", "2", "4", "6", "8"],
+    "6": ["6", "3", "5", "9"],
+
+    "7": ["7", "4", "8"],
+    "8": ["8", "5", "7", "9", "0"],
+    "9": ["9", "6", "8"],
+
+    "0": ["0", "8"] 
+  };
+  
+  let hasil = [""];
+  for(let i = 0; i < observed.length; i++){
+    
+    let hasilbaru = []
+    for(const old of hasil){
+      for(const digits of keyPad[observed[i]]){
+        hasilbaru.push(old + digits)
+      }
+    }
+    hasil = hasilbaru
+  }
+  return hasil
+} 
